2d0b66553f8cbc12168f48c9b9731ecaff1b186c
realpitstop <reelpitstop@gmail.com>
2026-03-10 22:29:48 -0500

    Fixed constants paths

--- text2sql/constants.py
+++ text2sql/constants.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 
-SCHEMA_PATH = "/text2sql/schema.json"
-GRAPH_PATH = "/text2sql/graph.pickle"
-STANCES_PATH = "/data/clean/stances.jsonl"
-SIC_MEANINGS = "/data/clean/sic_meaning.json"
+SCHEMA_PATH = "./text2sql/schema.json"
+GRAPH_PATH = "./text2sql/graph.pickle"
+STANCES_PATH = "./data/clean/stances.jsonl"
+SIC_MEANINGS = "./data/clean/sic_meaning.json"
